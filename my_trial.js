fc694d33b4adeaef0e09fb43ae7a7f4626a87f65
Siggy Hinds <siggy.hinds@olioapps.com>
2018-08-08 23:03:22 -0700

    implemented: removeListener, removeAllListeners

--- my_trial.js
+++ my_trial.js
@@ -2,9 +2,29 @@ import EventEmitter from "./src/event_emitter"
 
 var myEmitter = new EventEmitter()
 
-console.log("*** Hey, this is your trial program... ")
+var fun1 = () => console.log("test 1 callback")
+var fun2 = () => console.log("test 2 callback")
 
+console.log("\n*** Hey, this is your unofficial \"test\" program... \n\n")
 
-myEmitter.on("test", () => console.log("test callback"))
+// on()
+console.log("* registering a function to \"test\"")
+myEmitter.on("test", fun1)
+console.log( myEmitter.listeners("test").toString() )
 
+console.log("* registering another function to \"test\"")
+myEmitter.on("test", fun2)
+console.log( myEmitter.listeners("test").toString() )
+
+console.log("* output of unregistered event")
+console.log( myEmitter.listeners("fake").toString() )
+
+// removeListener()
+console.log("* remove fun1 from \"test\"")
+myEmitter.removeListener("test", fun1)
+console.log( myEmitter.listeners("test").toString() )
+
+// removeAllListeners
+console.log("* Remove all for \"test\"")
+myEmitter.removeAllListeners("test")
 console.log( myEmitter.listeners("test").toString() )
--- src/event_emitter.js
+++ src/event_emitter.js
@@ -11,13 +11,17 @@ export default class EventEmitter {
     constructor() {
         this._eventMap = new Map()   // "eventName" -> Function[]
 
+        // this.emit = this.emit.bind(this)
         this.on = this.on.bind(this)
+        // this.once = this.emit.once(this)
+        this.removeListener = this.removeListener.bind(this)
+        this.removeAllListeners = this.removeAllListeners.bind(this)
         this.listeners = this.listeners.bind(this)
     }
 
     // Methods //
 
-    /**
+    /** TODO
      * emit()
      * Call to trigger a named event with any number of arguments.
      * @param {string} eventName - The name of the event to trigger
@@ -42,15 +46,15 @@ export default class EventEmitter {
      *     });
      */
     on(eventName, listener) {
-        if (this._eventMap[eventName]) {
-            this._eventMap[eventName].push(listener)
+        if (this._eventMap.get(eventName)) {
+            this._eventMap.set(eventName, [...this._eventMap.get(eventName), listener])
         } else {
-           this._eventMap.set(eventName, [listener])
+            this._eventMap.set(eventName, [listener])
         }
     }
 
 
-    /**
+    /** TODO
      * once()
      * Call to register "listener" to a named event.
      * The "listener" will be called, at most, one time.
@@ -70,26 +74,35 @@ export default class EventEmitter {
      *
      * ex: myEmitter.removeListener('event', () => {console.log('an event occurred!') });
      */
-    removeListener(eventName, listener) { }
+    removeListener(eventName, listener) {
+        const listeners = this._eventMap.get(eventName)
+        if (listeners) {
+            this._eventMap.set(eventName, listeners.filter((elm) => elm !== listener))
+        }
+    }
 
 
     /**
      * removeAllListeners()
      * removes all "listener" from the named event
      * @param {string} eventName - The name of the event
+     * @return {boolean} - returns true if removed, false if event name is not registered
      *
      * ex: myEmitter.removeAllListeners('event')
      */
-    removeAllListeners(eventName) { }
+    removeAllListeners(eventName) {
+        return this._eventMap.delete(eventName)
+    }
 
     /**
      * listeners()
      * for testing...
      * Returns an array of listeners registered to the named event
      * @param {string} eventName - The name of the event
-     * @return {Function[]} - array of listeners registered to the event
+     * @return {Function[] | false} - array of listeners registered to the event, or false if event does is not registered
      */
     listeners(eventName) {
-        return this._eventMap.get(eventName)
+        var listeners = this._eventMap.get(eventName)
+        return !!listeners ? listeners : false
     }
 }
