f925358b69bcbcc2a58f94997940f2f84cdf6a79
Siggy Hinds <siggy.hinds@olioapps.com>
2018-08-14 20:09:54 -0700

    added tests for emit() and once()

--- src/test/event_emitter.test.js
+++ src/test/event_emitter.test.js
@@ -112,20 +112,104 @@ describe("EventEmitter", () => {
       })
     })
 
-    // describe("emit()", () => {}
-    // describe("once()", () => {}
+    describe("emit()", () => {
+        test("given a registered namedEvent, expect emit to return true", () => {
+          const callFunctionMock = jest.fn();
+          myEmitter.on(testE1, callFunctionMock)
+          const result = myEmitter.emit(testE1)
+          expect(result).toBe(true);
+        })
+
+        test("given an unregistered namedEvent, expect emit to return false", () => {
+          const result = myEmitter.emit(testE1)
+          expect(result).toBe(false);
+        })
+
+        test("given a registered listener w/no-args, expect listener to have been called", () => {
+          const callFunctionMock = jest.fn();
+          myEmitter.on(testE1, callFunctionMock)
+          myEmitter.emit(testE1)
+          expect(callFunctionMock).toBeCalled();
+        })
+
+        test("given a registered listener with args, expect emit() to have called listener with args", () => {
+          const callFunctionMock = jest.fn(fun3)
+          const args = [0,1]
+          myEmitter.on(testE1, callFunctionMock)
+          myEmitter.emit(testE1, ...args)
+          expect(callFunctionMock).toBeCalled()
+          expect(callFunctionMock.mock.calls.length).toBe(1)
+          // The first argument of the first call to the function
+          expect(callFunctionMock.mock.calls[0][0]).toEqual(...args)
+        })
+
+        test("given a registered namedEvent with multiple listeners no-args, expect emit() to have called each listener", () => {
+          const callFunctionMock = jest.fn()
+          const callFunctionMock2 = jest.fn()
+          myEmitter.on(testE1, callFunctionMock)
+          myEmitter.on(testE1, callFunctionMock2)
+
+          myEmitter.emit(testE1)
+          expect(callFunctionMock).toBeCalled()
+          expect(callFunctionMock2).toBeCalled()
+        })
+
+        test("given a registered namedEvent with multiple listeners w/ args, expect emit() to have called each listener with given args", () => {
+          const callFunctionMock = jest.fn(fun3)
+          const callFunctionMock2 = jest.fn(fun4)
+          const args = [3,4]
+          myEmitter.on(testE1, callFunctionMock)
+          myEmitter.on(testE1, callFunctionMock2)
+
+          myEmitter.emit(testE1, ...args)
+          expect(callFunctionMock).toBeCalled();
+          expect(callFunctionMock2).toBeCalled();
+
+          expect(callFunctionMock.mock.calls.length).toBe(1)
+          expect(callFunctionMock.mock.calls[0][0]).toEqual(...args)
+
+          expect(callFunctionMock2.mock.calls.length).toBe(1)
+          expect(callFunctionMock2.mock.calls[0][0]).toEqual(...args)
+        })
+
+        test("calling emit() with a namedEvent does call listeners of other events", () => {
+          const callFunctionMock = jest.fn(fun1)
+          const callFunctionMock2 = jest.fn(fun2)
+          myEmitter.on(testE1, callFunctionMock)
+          myEmitter.on(testE2, callFunctionMock2)
+
+          myEmitter.emit(testE1)
+          expect(callFunctionMock).toBeCalled();
+          expect(callFunctionMock2).not.toBeCalled();
+        })
+    })
+
+    describe("once()", () => {
+      test('a listener registered with once(), is only called once on emit()', () => {
+        const callFunctionMock = jest.fn(fun1)
+        myEmitter.once(testE1, callFunctionMock)
+
+        myEmitter.emit(testE1)
+        expect(callFunctionMock).toBeCalled()
+        expect(callFunctionMock.mock.calls.length).toBe(1)
+      })
+
+      test('a listener registered with once(), is not registered after emit() has been called', () => {
+        const callFunctionMock = jest.fn(fun1)
+        myEmitter.once(testE1, callFunctionMock)
+
+        const actual = myEmitter.listeners(testE1)
+
+        expect(actual).toHaveLength(1)
+
+        myEmitter.emit(testE1)
+        expect(callFunctionMock).toBeCalled()
+        expect(callFunctionMock.mock.calls.length).toBe(1)
+
+        const actual2 = myEmitter.listeners(testE1)
+
+        expect(actual2).toHaveLength(0)
+        expect(actual2).not.toContain(fun1)
+      })
+    })
 })
-
-// describe('<Gallery />', () => {
-//   it('Expect sendMessageToParentWindow to be called on image change', () => {
-//     const sendEventToParentWindowMock = jest.fn();
-//     const onChangeImageMock = jest.fn(() => {
-//          sendEventToParentWindowMock();
-//     });
-
-//     const gallery = shallow(<Gallery images={imagesMockData} onChange={onChangeImageMock} />); // Passing the mocked onChangeImage as prop
-//     gallery.find('input#image-1').simulate('change');
-
-//     expect(sendEventToParentWindowMock).toBeCalled();
-//   });
-// }
